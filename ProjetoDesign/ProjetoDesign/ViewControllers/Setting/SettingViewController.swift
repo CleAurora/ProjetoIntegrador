2ad766286e8a1272beebc3f3cb10187ddf61f1fa
Cleís Aurora Silveira Pereira <cleispereira@intelitrader.com.br>
2020-11-24 07:32:50 -0300

    Add Change photo in ProfileSetting

--- ProjetoDesign/ProjetoDesign/ViewControllers/Setting/SettingViewController.swift
+++ ProjetoDesign/ProjetoDesign/ViewControllers/Setting/SettingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+final class SettingViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var settingImageView: UIImageView!
 
     override func viewDidLoad() {
@@ -18,6 +18,30 @@ class SettingViewController: UIViewController {
                 [UIRectCorner.bottomLeft, UIRectCorner.bottomRight]
             ).rawValue
         )
+    }
+
+    @IBAction func changePictureButtonTapped() {
+        let imagePicker = UIImagePickerController()
+
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.modalPresentationStyle = .fullScreen
+        imagePicker.delegate = self
+
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let chosenImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            settingImageView.contentMode = .scaleAspectFill
+            settingImageView.image = chosenImage
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
 
