35d02d3a3376cb47bb9c66ae52c92f3efc4dae6c
Cleís Aurora Silveira Pereira <cleispereira@intelitrader.com.br>
2020-12-19 22:16:16 -0300

    Fix duplicate poster in feed and fix Register and add PKHUD

--- ProjetoDesign/ProjetoDesign/ViewControllers/Registrar/RegisterViewModel.swift
+++ ProjetoDesign/ProjetoDesign/ViewControllers/Registrar/RegisterViewModel.swift
@@ -11,17 +11,17 @@ import FirebaseAuth
 import FirebaseDatabase
 import FirebaseStorage
 
-class RegisterViewModel: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
-    var ref: DatabaseReference!
+final class RegisterViewModel: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    let ref: DatabaseReference = Database.database().reference()
     var register = RegistrarViewController()
     var imageSelected: UIImage?
+    var uid: String? { Auth.auth().currentUser?.uid }
 
-    var uid = Auth.auth().currentUser?.uid
     init(view: RegistrarViewController) {
         self.register = view
     }
-    @objc func click()
-       {
+
+    @objc func click() {
         let imagePicker = UIImagePickerController()
         imagePicker.allowsEditing = false
         imagePicker.sourceType = .photoLibrary
@@ -29,60 +29,59 @@ class RegisterViewModel: NSObject, UINavigationControllerDelegate, UIImagePicker
         imagePicker.delegate = self
         register.present(imagePicker, animated: true, completion: nil)
 
-       }
-       @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if  let chosenImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage{
-            register.userImage.contentMode = .scaleAspectFill
-            register.userImage.image = chosenImage
-            imageSelected = chosenImage
+   }
 
-           }
-        register.dismiss(animated: true, completion: nil)
-       }
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    if  let chosenImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage{
+        register.userImage.contentMode = .scaleAspectFill
+        register.userImage.image = chosenImage
+        imageSelected = chosenImage
+    }
 
-       func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    register.dismiss(animated: true, completion: nil)
+   }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         register.dismiss(animated: true, completion: nil)
-       }
+    }
 
     func isDeveloping(){
         let alert = UIAlertController(title: "This option still under development", message: "", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         register.present(alert, animated: true, completion: nil)
     }
+
     func registerNewUser(completionHandler: @escaping (_ result: Bool, _ error: Error?) -> Void){
         if register.emailTextField.text! != nil && register.emailTextField.text! != "" && register.passwordTextField.text! != nil && register.passwordTextField.text! != "" && register.secureTextField.text! != nil && register.secureTextField.text! != "" {
 
             if register.passwordTextField.text == register.secureTextField.text {
                 Auth.auth().createUser(withEmail: register.emailTextField.text!, password: register.passwordTextField.text!) { authResult, error in
-
                     if error != nil {
-                        print("erro ao registrar")
-
-                    }else{
-                        self.saveFIRData()
-                        completionHandler(true,nil)
+                        completionHandler(false, error)
+                    } else {
+                        self.saveFIRData(completionHandler: completionHandler)
                     }
                 }
             }
-        }else {
-            completionHandler(false,nil)
+        } else {
+            completionHandler(false, nil)
         }
-
     }
-    func saveFIRData() {
-        self.uploadImage(register.userImage.image!) { url in
+
+    func saveFIRData(completionHandler: @escaping (_ result: Bool, _ error: Error?) -> Void) {
+        uploadImage(register.userImage.image!) { (url, error) in
             if url != nil {
-                self.saveImage(name: "", profileURL: url!) { success in
-                    if success != nil{
-                        print("Yeah")
-                    }
+                return self.saveImage(name: "", profileURL: url!) { (url, error) in
+                    completionHandler(url != nil, error)
                 }
             }
+
+            completionHandler(false, error)
         }
     }
 }
 extension RegisterViewModel {
-    func uploadImage(_ image:UIImage, completion: @escaping ((_ url: URL?) -> ())){
+    func uploadImage(_ image:UIImage, completionHandler: @escaping (_ result: URL?, _ error: Error?) -> Void){
         let storageRef = Storage.storage().reference().child("\(register.nicknameTextField.text!).png")
         let imgData = register.userImage.image?.pngData()
         let metaData = StorageMetadata()
@@ -91,40 +90,41 @@ extension RegisterViewModel {
             if error == nil{
                 print("success")
                 storageRef.downloadURL(completion: { (url, error) in
-                    completion(url)
+                    completionHandler(url, nil)
                 })
             }else {
                 print("error in save image")
-                completion(nil)
+                completionHandler(nil, error)
             }
         }
     }
-    func saveImage(name: String, profileURL:URL, completion: @escaping ((_ url: URL?) -> ())) {
-        if let currentUserID = Auth.auth().currentUser?.uid{
-            self.ref = Database.database().reference()
-            let dict = ["Name": register.nameTextField.text!,"Email": register.emailTextField.text!, "profileUrl":profileURL.absoluteString,"UserID": currentUserID,"Nickname": register.nicknameTextField.text!, "Bio": "", "Followers": 0, "Following": 0] as [String: Any]
-            ref.child("users").child(currentUserID).setValue(dict)
 
-            self.setFollower()
-            self.setFollowing()
-        }
-    }
-    func setFollower(){
-        if let currentUser = Auth.auth().currentUser?.uid{
-            self.ref = Database.database().reference()
-            let followers = ref.child("users").child(currentUser).child("followers")
-            let dict = ["first": ""]
-            followers.updateChildValues(dict)
+    func saveImage(name: String, profileURL:URL, completionHandler: @escaping (_ result: URL?, _ error: Error?) -> Void) {
+        if let currentUserID = uid {
+            let dict: [String: Any] = [
+                "Name": register.nameTextField.text!,
+                "Email": register.emailTextField.text!,
+                "profileUrl": profileURL.absoluteString,
+                "UserID": currentUserID,
+                "Nickname": register.nicknameTextField.text!,
+                "Bio": "",
+                "Followers": 0,
+                "Following": 0,
+                "followers": [
+                    "first": " "
+                ],
+                "following": [
+                    "first": " "
+                ]
+            ]
 
-        }
-    }
-    func setFollowing(){
-        if let currentUser = Auth.auth().currentUser?.uid{
-            self.ref = Database.database().reference()
-            let following = ref.child("users").child(currentUser).child("following")
-            let dict = ["first": ""]
-            following.updateChildValues(dict)
+            ref.child("users").child(currentUserID).setValue(dict) { (error, _) in
+                if let error = error {
+                    return completionHandler(nil, error)
+                }
 
+                completionHandler(profileURL, nil)
+            }
         }
     }
 }
