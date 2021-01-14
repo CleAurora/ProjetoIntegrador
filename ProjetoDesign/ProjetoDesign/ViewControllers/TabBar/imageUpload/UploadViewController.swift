7e5c8204cdb7d6f9a2f8ca75e7bf95c8db361da3
Cleís Aurora Silveira Pereira <cleispereira@intelitrader.com.br>
2021-01-14 09:48:52 -0300

    Add internet error in feed and upload

--- ProjetoDesign/ProjetoDesign/ViewControllers/TabBar/imageUpload/UploadViewController.swift
+++ ProjetoDesign/ProjetoDesign/ViewControllers/TabBar/imageUpload/UploadViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Reachability
+import PKHUD
 
 class UploadViewController: UIViewController,  UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
@@ -22,16 +24,40 @@ class UploadViewController: UIViewController,  UIImagePickerControllerDelegate, 
     // MARK: - Proprierts
     var uploadArray = [Upload]()
 
+    private let reachability = try! Reachability()
+    private var hasInternet: Bool = true
+
+    deinit {
+        reachability.stopNotifier()
+    }
+
     // MARK: - Super Methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         imageViewBorder.roundedBottom()
+        setupReachability()
     }
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         uploadData.photosArray.removeAll()
         grabPhotos()
     }
+
+    private func setupReachability() {
+        reachability.whenReachable = { [self] _ in
+            hasInternet = true
+        }
+
+        reachability.whenUnreachable = { [self] _ in
+            hasInternet = false
+        }
+
+        try? reachability.startNotifier()
+    }
+
     func grabPhotos(){
         uploadData.grabPhotos(completionHandler: { success, _ in
             if success {
@@ -46,7 +72,16 @@ class UploadViewController: UIViewController,  UIImagePickerControllerDelegate, 
         uploadCollectionView.reloadData()
     }
     // MARK: - IBActions
+
+    private func showInternetError() {
+        HUD.flash(.labeledError(title: "No internet", subtitle: "You can't continue"), delay: 5)
+    }
+
     @IBAction func nextAction(_ sender: Any) {
+        guard hasInternet else {
+            return showInternetError()
+        }
+
         if let legendViewController = UIStoryboard(name: "Legend", bundle: nil).instantiateInitialViewController() as? LegendViewController {
         legendViewController.imagemProfile = resizeImageView.image
 
