492590252b615db576ce8c54d2940de6da7f8394
Mauro Juliano <43898163+MauroJuliano@users.noreply.github.com>
2021-01-15 16:26:59 -0800

    fix the button google design

--- ProjetoDesign/ProjetoDesign/ViewControllers/Login/ViewController.swift
+++ ProjetoDesign/ProjetoDesign/ViewControllers/Login/ViewController.swift
@@ -27,14 +27,19 @@ class ViewController: UIViewController {
         backgroundImageView.image = UIImage(named: "login2")
         backgroundImageView.layer.maskedCorners = CACornerMask(rawValue: UIRectCorner([.bottomLeft, .bottomRight]).rawValue)
         loginButton.backgroundColor = UIColor(patternImage: UIImage(named: "2.jpg")!)
+        
+        googleButtonTapped.isHidden = true
+        
         setupView()
-
         setupUI()
-        if GIDSignIn.sharedInstance()?.currentUser != nil {
-            <#code#>
-        }
         GIDSignIn.sharedInstance()?.presentingViewController = self
-        //GIDSignIn.sharedInstance()?.signIn()
+//        if GIDSignIn.sharedInstance()?.currentUser != nil {
+//            //
+//        }else {
+//            GIDSignIn.sharedInstance()?.signIn()
+//        }
+        
+        
     }
     override func viewDidAppear(_ animated: Bool) {
         self.loginModel = LoginViewModel(view: self)
@@ -46,6 +51,13 @@ class ViewController: UIViewController {
                 }
             }
         })
+        
+        if GIDSignIn.sharedInstance()?.currentUser != nil {
+            print("logou")
+        }else {
+            GIDSignIn.sharedInstance()?.signIn()
+        }
+        
     }
 
     func setupUI(){
@@ -53,6 +65,7 @@ class ViewController: UIViewController {
         self.loginButton.setTitle(loginModel?.titleBtnLogin, for: .normal )
         self.registerButton.setTitle(loginModel?.titleBtnRegister, for: .normal)
         self.faceButtonTapped.setImage(UIImage(named: loginModel!.imageBtnFaceBook), for: .normal)
+        
         //self.googleButtonTapped.setImage(UIImage(named: loginModel!.imageBtnInstagram), for: .normal)
     }
 
@@ -85,70 +98,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func faceButtonTapped(_ sender: Any) {
-        showUnderDevelopment()
+        
     }
 
     @IBAction func googleButtonTapped(_ sender: Any) {
-        //showUnderDevelopment()
-
-//        func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
-//            if let error = error {
-//                return
-//            }
-//
-//            guard let authentication = user.authentication else { return }
-//            let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
-//        }
+        print("pressionado")
+        GIDSignIn.sharedInstance()?.signIn()
     }
-
-//    func loginWithGoogle(credential: AuthCredential, completionHandler: @escaping (_ result: Bool, _ error: Error?) -> Void) {
-//        Auth.auth().signIn(with: credential) { (authResult, error) in
-//          if let error = error {
-//            let authError = error as NSError
-//            if (isMFAEnabled && authError.code == AuthErrorCode.secondFactorRequired.rawValue) {
-//              // The user is a multi-factor user. Second factor challenge is required.
-//              let resolver = authError.userInfo[AuthErrorUserInfoMultiFactorResolverKey] as! MultiFactorResolver
-//              var displayNameString = ""
-//              for tmpFactorInfo in (resolver.hints) {
-//                displayNameString += tmpFactorInfo.displayName ?? ""
-//                displayNameString += " "
-//              }
-//              self.showTextInputPrompt(withMessage: "Select factor to sign in\n\(displayNameString)", completionBlock: { userPressedOK, displayName in
-//                var selectedHint: PhoneMultiFactorInfo?
-//                for tmpFactorInfo in resolver.hints {
-//                  if (displayName == tmpFactorInfo.displayName) {
-//                    selectedHint = tmpFactorInfo as? PhoneMultiFactorInfo
-//                  }
-//                }
-//                PhoneAuthProvider.provider().verifyPhoneNumber(with: selectedHint!, uiDelegate: nil, multiFactorSession: resolver.session) { verificationID, error in
-//                  if error != nil {
-//                    print("Multi factor start sign in failed. Error: \(error.debugDescription)")
-//                  } else {
-//                    self.showTextInputPrompt(withMessage: "Verification code for \(selectedHint?.displayName ?? "")", completionBlock: { userPressedOK, verificationCode in
-//                      let credential: PhoneAuthCredential? = PhoneAuthProvider.provider().credential(withVerificationID: verificationID!, verificationCode: verificationCode!)
-//                      let assertion: MultiFactorAssertion? = PhoneMultiFactorGenerator.assertion(with: credential!)
-//                      resolver.resolveSignIn(with: assertion!) { authResult, error in
-//                        if error != nil {
-//                          print("Multi factor finanlize sign in failed. Error: \(error.debugDescription)")
-//                        } else {
-//                          self.navigationController?.popViewController(animated: true)
-//                        }
-//                      }
-//                    })
-//                  }
-//                }
-//              })
-//            } else {
-//              self.showMessagePrompt(error.localizedDescription)
-//              return
-//            }
-//            // ...
-//            return
-//          }
-//          // User is signed in
-//          // ...
-//        }
-//    }
 
     private func showUnderDevelopment(){
         let alert = UIAlertController(
