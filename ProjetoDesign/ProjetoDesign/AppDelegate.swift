8e10793010ac497f709fb35ce4c0e4f5154109a6
Cleís Aurora Silveira Pereira <cleispereira@intelitrader.com.br>
2021-01-17 13:57:27 -0300

    Add support for offline capabilities for Firebase Realtime Database

--- ProjetoDesign/ProjetoDesign/AppDelegate.swift
+++ ProjetoDesign/ProjetoDesign/AppDelegate.swift
@@ -5,19 +5,24 @@
 //  Created by Lestad on 2020-10-04.
 //
 
-import UIKit
 import IQKeyboardManagerSwift
 import Firebase
+import UIKit
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
+    // MARK: - UIApplicationDelegate conforms
+
     func applicationDidFinishLaunching(_ application: UIApplication) {
         UITabBar.appearance().unselectedItemTintColor = UIColor.green
+
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+
         FirebaseApp.configure()
 
+        Database.database().isPersistenceEnabled = true
     }
 }
